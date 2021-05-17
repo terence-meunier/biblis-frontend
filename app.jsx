d5da9867f851336b1d2a76275ad359c89eb9d87c
Térence MEUNIER <terence.meunier@gmail.com>
2021-05-17 15:59:23 +0200

    feat(findRecord): Add Component to find record

--- app.jsx
+++ app.jsx
@@ -1,4 +1,5 @@
-function Record(props) {
+// Element de construction de Record
+function Element(props) {
     return (
         <ul>
             <li>Auteur : {props.author}</li>
@@ -8,6 +9,7 @@ function Record(props) {
     )
 }
 
+// Element lorsque plusieurs elements sont retournés
 class Records extends React.Component {
     constructor(props) {
         super(props);
@@ -42,8 +44,8 @@ class Records extends React.Component {
                     {this.state.records.map((record) => {
                         return (
                         <div key={record.recordID}>
-                            <h2>{record.recordTitle}</h2>
-                            <Record  
+                            <a href={"?record=" + record.recordID}><h2>{record.recordTitle}</h2></a>
+                            <Element  
                             author={record.author}
                             editor={record.editor}
                             createdDate={record.createdDate}
@@ -57,4 +59,55 @@ class Records extends React.Component {
     }
 }
 
-ReactDOM.render(<Records />, document.querySelector("#app"));
+// Element lorsqu'un seul element est retourné
+class Record extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            record: null
+        };
+    }
+
+    componentDidMount() {
+        window.fetch("http://localhost:4567/" + this.props.id)
+        .then((res) => res.json())
+        .then((datas) => {
+            console.log(datas);
+            this.setState({
+                record: datas
+            });
+        })
+        .catch((err) => console.error(err));
+    }
+
+    render() {
+
+        if (this.state.record == null) {
+            return (
+            <h2>
+                Chargement...
+            </h2>
+            );
+        } else {
+            return (
+                <div>
+                    <h2>{this.state.record.recordTitle}</h2>
+                    <Element  
+                    author={this.state.record.author}
+                    editor={this.state.record.editor}
+                    createdDate={this.state.record.createdDate}
+                    />
+                </div>
+            );
+        }
+    }
+}
+
+// On parse l'url
+const urlData = document.URL.split("?")[1];
+if (urlData != null) {
+    const record = urlData.split("=")[1];
+    ReactDOM.render(<Record id={record} />, document.querySelector("#app"));
+} else {
+    ReactDOM.render(<Records />, document.querySelector("#app"));
+}
